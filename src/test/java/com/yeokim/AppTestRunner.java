7a00ba1a9ab1a2dcf4de6ad55eb8732156358157
duswnsxnxn <duswnsxnxn@gmail.com>
2023-03-07 06:16:48 +0900

    AppTestRunner 도입해서, 중복제거

---
+++ src/test/java/com/yeokim/AppTestRunner.java
@@ -0,0 +1,23 @@
+package com.yeokim;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+public class AppTestRunner {
+
+
+    public static String run(String cmd) {
+
+        cmd.stripIndent().trim();
+        cmd += "\n종료";
+        Scanner sc = TestUtil.genScanner(cmd);
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String s = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        return s;
+    }
+}
